35edf08537089cd8374ae2667e824f109d2a4cfc
BuildTools <unconfigured@null.spigotmc.org>
2017-08-27 02:20:29 +0200

    New anims

--- src/Game/Game.lua
+++ src/Game/Game.lua
@@ -1,6 +1,5 @@
 local Background    = require "src/Objects/Background"
 local Hero          = require "src/Game/Hero"
-local Object_Box    = require "src/Objects/Object_Box"
 
 local Game = {}
 
@@ -8,9 +7,8 @@ function Game:Initialize()
     love.physics.setMeter( 100 )
     world = love.physics.newWorld( 0, 9.81 * love.physics.getMeter(), true ) --normal gravity
 
-    hero1 = Hero:New( world, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 0 )
-    object_box1 = Object_Box:New( world, 200, 50, 0 )
-    object_box2 = Object_Box:New( world,600, -80, 0 )
+    hero1 =  Hero:New( world, 50, love.graphics.getHeight() - 300, 0 )
+    hero2 = Hero:New( world, 50, 50, 1 )
 
     backgrounds = {}
     foregrounds = {}
@@ -22,7 +20,13 @@ function Game:Initialize()
     floor.body = love.physics.newBody( world, love.graphics.getWidth() / 2, love.graphics.getHeight() - 10, "static" )
     floor.shape = love.physics.newRectangleShape( 3000, 10 )
     floor.fixture = love.physics.newFixture( floor.body, floor.shape )
-    floor.fixture:setFriction( 1.0 )
+    floor.fixture:setFriction( 0.0 )
+
+    floor2 = {}
+    floor2.body = love.physics.newBody( world, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2 - 10, "static" )
+    floor2.shape = love.physics.newRectangleShape( 3000, 10 )
+    floor2.fixture = love.physics.newFixture( floor2.body, floor2.shape )
+    floor2.fixture:setFriction( 0.0 )
 end
 
 function Game:Draw()
@@ -31,14 +35,17 @@ function Game:Draw()
         v:Draw()
     end
 
-    object_box1:Draw()
-    object_box2:Draw()
-
-
     hero1:Draw()
-    --hero2:Draw()
+    hero2:Draw()
     x, y, x2, y2 = floor.shape:computeAABB( 0, 0, 0 )
     x, y, x2, y2 = floor.body:getWorldPoints( x, y, x2, y2 )
+    x, y = Camera.MapToScreen( x, y )
+    x2, y2 = Camera.MapToScreen( x2, y2 )
+
+    love.graphics.rectangle( "fill", x, y, x2-x, y2-y )
+
+    x, y, x2, y2 = floor2.shape:computeAABB( 0, 0, 0 )
+    x, y, x2, y2 = floor2.body:getWorldPoints( x, y, x2, y2 )
     x, y = Camera.MapToScreen( x, y )
     x2, y2 = Camera.MapToScreen( x2, y2 )
 
@@ -56,12 +63,8 @@ function Game:Update( dt )
         v:Update( dt )
     end
 
-    object_box1:Update( dt )
-    object_box2:Update( dt )
-
-
     hero1:Update( dt )
-    --hero2:Update( dt )
+    hero2:Update( dt )
     world:update( dt )
 
     for k,v in pairs( foregrounds ) do
@@ -72,12 +75,12 @@ end
 
 function Game:KeyPressed( key, scancode, isrepeat )
     hero1:KeyPressed( key, scancode, isrepeat )
-    --hero2:KeyPressed( key, scancode, isrepeat )
+    hero2:KeyPressed( key, scancode, isrepeat )
 end
 
 function Game:KeyReleased( key, scancode )
     hero1:KeyReleased( key, scancode )
-    --hero2:KeyPressed( key, scancode )
+    hero2:KeyReleased( key, scancode )
 end
 
 function  Game:mousepressed( iX, iY, iButton, iIsTouch )
--- src/Game/Hero.lua
+++ src/Game/Hero.lua
@@ -24,21 +24,35 @@ function Hero:New( world, x, y, type )
     newHero.fixture:setFriction( 1.0 )
     newHero.animations = {}
     newHero.currentAnimation = 0
-
+    newHero.canJump = false
+    newHero.type = type
+    
     --Hero values
-    newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 14, 24, 0, 0, 529, 694, false, false )
-    newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 14, 24, 0, 0, 529, 694, true, false )
-    --newHero:AddAnimation( "runsprite.png", 5, 3, 0, 150, 120, 150 )
-    newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 1, 24, 0, 0, 529, 694, false, false )
-    newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 1, 24, 0, 0, 529, 694, true, false )
+    if type == 0 then
+        newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 14, 24, 0, 0, 529, 694, false, false )--1
+        newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 14, 24, 0, 0, 529, 694, true, false )--2
+        newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 1, 24, 0, 0, 529, 694, false, false )--3
+        newHero:AddAnimation( "resources/Animation/Characters/singe-course.png", 1, 24, 0, 0, 529, 694, true, false )--4
+        newHero:AddAnimation( "resources/Animation/Characters/singe-inactif-tout.png", 18, 24, 0, 0, 583, 667, false, false )--5
+        newHero:AddAnimation( "resources/Animation/Characters/singe-inactif-tout.png", 18, 24, 0, 0, 583, 667, true, false )--6
+    elseif type == 1 then
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-course.png", 14, 24, 0, 0, 548, 826, false, false )
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-course.png", 14, 24, 0, 0, 548, 826, true, false )
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-course.png", 1, 24, 0, 0, 548, 826, false, false )
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-course.png", 1, 24, 0, 0, 548, 826, true, false )
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-inactif-tout.png", 17, 24, 0, 0, 526, 909, false, false )--5
+        newHero:AddAnimation( "resources/Animation/Characters/lapin-inactif-tout.png", 17, 24, 0, 0, 526, 909, true, false )--6
+    end
     --newHero:AddAnimation( "runsprite.png", 1, 3, 0, 150, 120, 150 )
-    newHero:SetCurrentAnimation( 1 )
+    newHero:SetCurrentAnimation( 5 )
 
     return newHero
 end
 
 function Hero:jump()
-    self.body:applyLinearImpulse( 0, -300 )
+    vx, vy = self.body:getLinearVelocity()
+    self.body:setLinearVelocity( vx, -400 )
+    self.canJump = false
 end
 
 function Hero:runRight()
@@ -57,19 +71,35 @@ function Hero:StopRunning()
     vx, vy = self.body:getLinearVelocity()
     self.body:setLinearVelocity( 0, vy )
     if vx >= 0 then
-        self:SetCurrentAnimation( 3 )
+        self:SetCurrentAnimation( 5 )
     end
     if vx < 0 then
-        self:SetCurrentAnimation( 4 )
+        self:SetCurrentAnimation( 6 )
     end
 end
 
 function Hero:Update( dt )
     -- Key holding detection
-    if love.keyboard.isDown( "left" ) then
-        self:runLeft()
-    elseif love.keyboard.isDown( "right" ) then
-        self:runRight()
+    if self.type == 0 then
+        if love.keyboard.isDown( "left" ) then
+            self:runLeft()
+        elseif love.keyboard.isDown( "right" ) then
+            self:runRight()
+        end
+    elseif self.type == 1 then
+        if love.keyboard.isDown( "q" ) then
+            self:runLeft()
+        elseif love.keyboard.isDown( "d" ) then
+            self:runRight()
+        end
+    end
+
+    local contacts = self.body:getContactList()
+    print(#contacts)
+    if #contacts > 0 then
+        self.canJump = true
+    else
+        self.canJump = false
     end
 
     self:UpdateObject( dt )
@@ -77,7 +107,7 @@ function Hero:Update( dt )
     x, y, x2, y2 = self.shape:computeAABB( 0, 0, 0 )
     x, y, x2, y2 = self.body:getWorldPoints( x, y, x2, y2 )
     Camera.x = x - love.graphics.getWidth() / 2
-    Camera.y = y - love.graphics.getHeight() / 2
+    Camera.y = 0 --love.graphics.getHeight() / 2
 end
 
 function Hero:Draw()
@@ -86,14 +116,32 @@ function Hero:Draw()
 end
 
 function Hero:KeyPressed( key, scancode, isrepeat )
+    if self.type == 0 then
+        if key == "up" and not isrepeat and self.canJump then
+            self:jump()
+        end
+    elseif self.type == 1 then
+        if key == "z" and not isrepeat and self.canJump then
+            self:jump()
+        end
+    end
 end
 
 function Hero:KeyReleased( key, scancode )
-    if key == "left" then
-        self:StopRunning()
-    end
-    if key == "right" then
-        self:StopRunning()
+    if self.type == 0 then
+        if key == "left" then
+            self:StopRunning()
+        end
+        if key == "right" then
+            self:StopRunning()
+        end
+    elseif self.type == 1 then
+        if key == "q" then
+            self:StopRunning()
+        end
+        if key == "d" then
+            self:StopRunning()
+        end
     end
 end
 
