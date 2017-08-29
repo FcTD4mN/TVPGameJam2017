28e3735a8e95395b7b0d916a7c074994fb21141f
FcTD4mN <fctwarrior@gmail.com>
2017-08-29 20:56:29 +0200

    Cleaned projectiles

--- src/Objects/Projectiles/Fireball.lua
+++ src/Objects/Projectiles/Fireball.lua
@@ -3,50 +3,64 @@ local Object = require "src/Objects/Object"
 
 local Fireball = Object:New( 0, 0, 0, 0, 0, 0, 0, 0 )
 
-function Fireball:New( world, x, y, iVelocity )
+
+-- ==========================================Constructor/Destructor
+
+
+function Fireball:New( iWorld, iX, iY, iVelocity )
     local newFireball = {}
     setmetatable( newFireball, self )
     self.__index = self
 
     newFireball.x = x
     newFireball.y = y
-
     newFireball.w = 90
     newFireball.h = 90
 
-    newFireball.flipNeeded = iVelocity < 0
 
     --inherited values
-    newFireball.body = love.physics.newBody( world, x + 45, y + 45, "dynamic" )
+    newFireball.body        = love.physics.newBody( iWorld, iX + newFireball.w/2, iY + newFireball.h/2, "dynamic" )
     newFireball.body:setFixedRotation( true )
     newFireball.body:setLinearVelocity( iVelocity, 0 )
     newFireball.body:setGravityScale( 0.0 )
-    newFireball.shape = love.physics.newRectangleShape( newFireball.w, newFireball.h )
-    newFireball.fixture = love.physics.newFixture( newFireball.body, newFireball.shape )
+
+    newFireball.shape       = love.physics.newRectangleShape( newFireball.w, newFireball.h )
+    newFireball.fixture     = love.physics.newFixture( newFireball.body, newFireball.shape )
     newFireball.fixture:setFriction( 1.0 )
     newFireball.fixture:setUserData( newFireball )
+
     newFireball.animations = {}
     newFireball.currentAnimation = 0
+    newFireball.flipNeeded = iVelocity < 0
 
     return newFireball
 end
 
-function Fireball:AddAnimation( iImage )
-    table.insert( self.animations, Animation:New( iImage, self.x, self.y, 90, 90, 0, 14, 24, self.flipNeeded, false ) )
+
+-- ==========================================Type
+
+function Fireball:Type()
+    return "Fireball"
 end
+
+
+-- ==========================================Update/Draw
 
 function Fireball:Update( dt )
     self:UpdateObject( dt )
 end
 
 function Fireball:Draw()
-    -- love.graphics.polygon( "fill", self.body:getWorldPoints( self.shape:getPoints() ) )
-
     self:DrawObject()
 end
 
-function Fireball:Type()
-    return "Fireball"
+
+-- ==========================================Fireball specific overrides
+
+
+function Fireball:AddAnimation( iImage )
+    table.insert( self.animations, Animation:New( iImage, self.x, self.y, self.w, self.h, 0, 14, 24, self.flipNeeded, false ) )
 end
+
 
 return Fireball
--- src/Objects/Projectiles/Waterball.lua
+++ src/Objects/Projectiles/Waterball.lua
@@ -3,50 +3,67 @@ local Object = require "src/Objects/Object"
 
 local Waterball = Object:New( 0, 0, 0, 0, 0, 0, 0, 0 )
 
-function Waterball:New( world, x, y, iVelocity )
+
+-- ==========================================Constructor/Destructor
+
+
+function Waterball:New( iWorld, iX, iY, iVelocity )
     local newWaterball = {}
     setmetatable( newWaterball, self )
     self.__index = self
 
     newWaterball.x = x
     newWaterball.y = y
-
     newWaterball.w = 90
     newWaterball.h = 90
 
-    newWaterball.flipNeeded = iVelocity < 0
 
     --inherited values
-    newWaterball.body = love.physics.newBody( world, x + 45, y + 45, "dynamic" )
+    newWaterball.body        = love.physics.newBody( iWorld, iX + newWaterball.w/2, iY + newWaterball.h/2, "dynamic" )
     newWaterball.body:setFixedRotation( true )
     newWaterball.body:setLinearVelocity( iVelocity, 0 )
     newWaterball.body:setGravityScale( 0.0 )
-    newWaterball.shape = love.physics.newRectangleShape( newWaterball.w, newWaterball.h )
-    newWaterball.fixture = love.physics.newFixture( newWaterball.body, newWaterball.shape )
+
+    newWaterball.shape       = love.physics.newRectangleShape( newWaterball.w, newWaterball.h )
+    newWaterball.fixture     = love.physics.newFixture( newWaterball.body, newWaterball.shape )
     newWaterball.fixture:setFriction( 1.0 )
     newWaterball.fixture:setUserData( newWaterball )
+
     newWaterball.animations = {}
     newWaterball.currentAnimation = 0
+    newWaterball.flipNeeded = iVelocity < 0
 
     return newWaterball
 end
 
-function Waterball:AddAnimation( iImage )
-    table.insert( self.animations, Animation:New( iImage, self.x, self.y, 90, 90, 0, 16, 24, self.flipNeeded, false ) )
+
+-- ==========================================Type
+
+
+function Waterball:Type()
+    return "Waterball"
 end
+
+
+-- ==========================================Update/Draw
+
 
 function Waterball:Update( dt )
     self:UpdateObject( dt )
 end
 
-function Waterball:Draw()
-    -- love.graphics.polygon( "fill", self.body:getWorldPoints( self.shape:getPoints() ) )
 
+function Waterball:Draw()
     self:DrawObject()
 end
 
-function Waterball:Type()
-    return "Waterball"
+
+-- ==========================================Waterball specific overrides
+
+
+function Waterball:AddAnimation( iImage )
+    table.insert( self.animations, Animation:New( iImage, self.x, self.y, self.w, self.h, 0, 16, 24, self.flipNeeded, false ) )
 end
+
 
 return Waterball
