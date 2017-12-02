6bbcdce0447bfb81537599e814e5f4e105313c81
FcTD4mN <fctwarrior@gmail.com>
2017-12-02 18:39:12 -0500

    Camera follows

--- src/Game/Level/Level1.lua
+++ src/Game/Level/Level1.lua
@@ -54,34 +54,34 @@ end
 -- ==========================================Type
 
 function  Level1:InitializeECS()
-    local hero = Entity:New( "hero" )
+    self.mHero = Entity:New( "hero" )
 
     -- Components
     local box2DComponent = BasicComponents:NewBox2DComponent( self.mWorld, 0, -100, 50, 50, "dynamic", true, 1 )
         local stickyShape    = love.physics.newRectangleShape( 50, 50 )
         local fixture  = love.physics.newFixture( box2DComponent.mBody, stickyShape )
         fixture:setFriction( 1.0 )
-        fixture:setUserData( hero )
+        fixture:setUserData( self.mHero )
 
     local animations = {}
     animations[ "idleright" ] = Animation:New( 'resources/Animation/Characters/Dummy/idle.png', 24, 16, true, false, false )
     animations[ "idleleft" ] = Animation:New( 'resources/Animation/Characters/Dummy/idle.png', 24, 16, true, true, false )
 
-    hero:AddComponent( BasicComponents:NewUserInput() )
-    hero:AddComponent( BasicComponents:NewStateComponent( "idleright" ) )
-    hero:AddComponent( BasicComponents:NewAnimationsComponent( animations ) )
-    --hero:AddComponent( BasicComponents:NewSimpleSprite( 'resources/Images/Objects/Water.png' ) )
-    hero:AddComponent( box2DComponent )
+    self.mHero:AddComponent( BasicComponents:NewUserInput() )
+    self.mHero:AddComponent( BasicComponents:NewStateComponent( "idleright" ) )
+    self.mHero:AddComponent( BasicComponents:NewAnimationsComponent( animations ) )
+    --self.mHero:AddComponent( BasicComponents:NewSimpleSprite( 'resources/Images/Objects/Water.png' ) )
+    self.mHero:AddComponent( box2DComponent )
 
-    hero:AddTag( "canJump" )
-    hero:AddTag( "isJumping" )
+    self.mHero:AddTag( "canJump" )
+    self.mHero:AddTag( "isJumping" )
 
     --local slipperyShape    = love.physics.newRectangleShape( self.mW - 25, self.mH - 5 )
     --fixture  = love.physics.newFixture( self.mBody, slipperyShape )
     --fixture:setFriction( 0.0 )
-    --fixture:setUserData( hero )
+    --fixture:setUserData( self.mHero )
 
-    ECSWorld:AddEntity( hero )
+    ECSWorld:AddEntity( self.mHero )
     ECSWorld:AddSystem( SpriteRenderer )
     ECSWorld:AddSystem( InputConverter )
     ECSWorld:AddSystem( AnimationRenderer )
@@ -110,19 +110,14 @@ end
 
 
 function  Level1:UpdateCamera()
-    if #self.mHeros <= 0 then
-        return
-    end
-    xAverage = 0
 
-    for k,v in pairs( self.mHeros ) do
-        x = v:GetX()
-        xAverage = ( xAverage + x )
-    end
-    xAverage = xAverage / #self.mHeros
+    if self.mHero ~= nil then
 
-    self.mCamera.mX = xAverage - love.graphics.getWidth() / 2
-    self.mCamera.mY = - ( love.graphics.getHeight() - 720 ) / 2
+        self.mCamera.mX = self.mHero:GetComponentByName( "box2d" ).mBody:getX() - self.mCamera.mW / 2
+        self.mCamera.mY = self.mHero:GetComponentByName( "box2d" ).mBody:getY() - self.mCamera.mH / 2
+
+    end
+
 end
 
 
